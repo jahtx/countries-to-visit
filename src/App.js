aacfad70cd5325bc964ea50bb923a39cfefa4158
James Hernandez <tech@jahtx.com>
2021-04-14 15:31:31 -0500

    go for it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/main.scss";
+import globe from "./images/spinning-globe.gif";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-left">
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="/">Travel Someplace</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Navbar>
+      {/* <div className="container-sm mt-3">
+        <p>"Wherever you go becomes a part of you somehow." - Anita Desai</p>
+      </div> */}
+      <div className="container-sm mt-3">
+        <img
+          src={globe}
+          className="pocketSizeImage"
+          alt="gratuitiously spinning globe"
+        />
+      </div>
+      <div className="container-sm">
+        <h4>Places where the weather suits your clothes:</h4>
+      </div>
     </div>
   );
 }
