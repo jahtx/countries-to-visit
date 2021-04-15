93e3c6596f5a2d7007f93e0a2fd0a2cb56d61b14
James Hernandez <tech@jahtx.com>
2021-04-15 11:30:09 -0500

    go

--- src/components/LinkList/LinkList.js
+++ src/components/LinkList/LinkList.js
@@ -1,11 +1,12 @@
-import React from "react";
-import Link from "../Link/Link";
+import * as React from "react";
+// import Link from "../Link/Link";
 import { gql, useQuery } from "@apollo/client";
 import { ApolloConsumer } from "@apollo/client";
-
+import Form from "react-bootstrap/Form";
 const GET_COUNTRIES = gql`
   query GetCountries {
     countries {
+      code
       name
       capital
     }
@@ -13,18 +14,49 @@ const GET_COUNTRIES = gql`
 `;
 
 const LinkList = () => {
+  /** Function that will set different values to state variable
+   * based on which dropdown is selected
+   */
+  const changeSelectOptionHandler = (event) => {
+    document.getElementById("capitalText").innerHTML = document.getElementById(
+      "country-selection"
+    ).value;
+  };
+
   const { loading, error, data } = useQuery(GET_COUNTRIES, {
     variables: { limit: 5 },
   });
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error!</div>;
+
   return (
     <ApolloConsumer>
-      {(client) =>
-        data.countries.map((country) => (
-          <Link key={country.id} country={country} />
-        ))
-      }
+      {(client) => (
+        <div>
+          <Form>
+            <Form.Group>
+              <Form.Label>Country to Visit</Form.Label>
+              <Form.Control
+                id="country-selection"
+                onChange={changeSelectOptionHandler}
+                as="select"
+                size="sm"
+                custom
+              >
+                {data.countries.map((country) => (
+                  <option key={country.code} value={country.capital}>
+                    {country.name}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Form>
+          <div>
+            You should visit the capital, which is &nbsp;"
+            <span id="capitalText"></span>".
+          </div>
+        </div>
+      )}
     </ApolloConsumer>
   );
 };
