fb455408306efc524cbc91097065a6c36b455830
James Hernandez <tech@jahtx.com>
2021-04-14 22:37:10 -0500

    sort of got ApolloProvider working

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import "./styles/main.scss";
 import globe from "./images/spinning-globe.gif";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-// import LinkList from "./components/LinkList/LinkList";
+import LinkList from "./components/LinkList/LinkList";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://graphql-weather-api.herokuapp.com/",
   cache: new InMemoryCache(),
@@ -54,28 +59,30 @@ client
   .then((result) => console.log(result));
 function App() {
   return (
-    <div className="App text-left">
-      <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="/">Oh The Places You'll Go!</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-      </Navbar>
+    <ApolloProvider client={client}>
+      <div className="App text-left">
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="/">Oh The Places You'll Go!</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Navbar>
 
-      <div className="container-sm mt-3">
-        <img
-          src={globe}
-          className="pocketSizeImage"
-          alt="gratuitiously spinning globe"
-        />
+        <div className="container-sm mt-3">
+          <img
+            src={globe}
+            className="pocketSizeImage"
+            alt="gratuitiously spinning globe"
+          />
+        </div>
+        <div className="container-sm">
+          <h4>Places where the weather suits your clothes:</h4>
+          <LinkList />
+        </div>
       </div>
-      <div className="container-sm">
-        <h4>Places where the weather suits your clothes:</h4>
-      </div>
-      <div className="container-sm">go</div>
-    </div>
+    </ApolloProvider>
   );
 }
 
