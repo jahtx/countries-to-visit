b1276c8195e267f308c909cb70e934970bff5ad9
James Hernandez <tech@jahtx.com>
2021-04-19 14:45:22 -0500

    small correction

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         </div>
       </div>
       <div className="container-sm text-center">
-        Country images made possible by
+        Country images made possible by&nbsp;
         <a id="unsplash-link" href="https://unsplash.com">
           Unsplash
         </a>
